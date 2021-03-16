cb67b443be42815d84f77a512038436e3526f39d
wangshuai <wangsbruce@163.com>
2021-03-16 16:21:25 +0800

    超链接

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import Link from 'next/link'
 export default function index() {
     return (
         <div>
-            首页
+            首页 <br/>
             <Link href="/list">列表</Link>
+            <br/>
+            <a href="http://www.baidu.com">连接</a>
         </div>
     )
 }
