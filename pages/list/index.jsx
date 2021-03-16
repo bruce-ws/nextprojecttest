0a6744464621257807900cd338b1d8a8e8aa779c
wangshuai <wangsbruce@163.com>
2021-03-16 15:44:16 +0800

    next项目提交

---
+++ pages/list/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function List() {
+    return (
+        <div>
+            对对对
+            <button onClick={ () => {
+                alert(0)
+            } }>button</button>
+        </div>
+    )
+}
